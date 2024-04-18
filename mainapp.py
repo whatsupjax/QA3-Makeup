6d7f4f3cd1b7b09ea437e663239f2772613f000a
whatsupjax <160513346+whatsupjax@users.noreply.github.com>
2024-04-18 17:08:17 -0500

    initialize rwSelectQuiz

--- mainapp.py
+++ mainapp.py
@@ -1,7 +1,25 @@
 import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
-mainSelectQuiz = tk.Tk()
+rwSelectQuiz = tk.Tk()
+root = rwSelectQuiz
 
+class SelectQuiz:
 
+    def __init__(self, root):
+        self.labSelectQuiz = tk.Label(root, text = "Select a quiz topic to be tested on.")
+        self.labSelectQuiz.grid()
 
-mainSelectQuiz.mainloop()
+        self.cbobxSelectQuiz = ttk.Combobox(root)
+        self.cbobxSelectQuiz.grid()
+
+        self.btnStartQuiz = tk.Button(root, text = 'Start Quiz')
+        self.btnStartQuiz.grid()
+
+    def comdCbobxEntry(self):
+        self.cbobxSelectQuiz.get()
+
+SelectQuiz(root)
+
+rwSelectQuiz.mainloop()
