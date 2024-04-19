5a7c0487d7beeaa01ce7f04593fcfa192f575a27
whatsupjax <160513346+whatsupjax@users.noreply.github.com>
2024-04-19 08:56:40 -0500

    create database

--- mainapp.py
+++ mainapp.py
@@ -1,20 +1,21 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+import sqlite3 as sql
 
 rwSelectQuiz = tk.Tk()
 root = rwSelectQuiz
 
 class SelectQuiz:
-
     def __init__(self, root):
         self.labSelectQuiz = tk.Label(root, text = "Select a quiz topic to be tested on.")
         self.labSelectQuiz.grid()
 
         self.cmbobxSelectQuiz = ttk.Combobox(root)
         self.cmbobxSelectQuiz.grid()
-
         self.cmbobxSelectQuiz['values'] = ('DS3850', 'DS3865', 'FIN3220', 'DS3841')
+        
+        self.courseVal = tk.StringVar(root)
 
         self.btnStartQuiz = tk.Button(root, text = 'Start Quiz', command = self.comdStartQuiz)
         self.btnStartQuiz.grid()
@@ -31,7 +32,7 @@ rwSelectQuiz.mainloop()
 
 rwQuizApp = tk.Tk()
 
-label = tk.Label(text = SelectQuiz.comdCmbobxEntry)
+label = tk.Label()
 label.grid()
 
 rwQuizApp.mainloop()
